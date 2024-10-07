dbc3c1acdfa25edef1ae696eb74cab041862a6f4
JoaoCussolim <joaovccussolim@gmail.com>
2024-10-06 22:44:11 -0300

    Tabs
    Initiated tabs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from tkcalendar import DateEntry
 import empresa as em
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
@@ -7,29 +9,55 @@ from datetime import datetime
 import calendar
 import os
 
-# Create an instance of the Empresa class
+# Instanciação do objeto da classe Empresa
 empresa = em.Empresa()
 
-# Create the main Tkinter window
+# Tela principal do tkinter
 root = tk.Tk()
-root.title("Organizar Calendário")
+
+# Instanciação do Notebook para criação de guias
+notebook = ttk.Notebook(root)
+
 root.geometry("800x600")
 icon_path = os.path.join(os.path.dirname(__file__), 'image.ico')
 root.iconbitmap(icon_path)
 
-# Label for the main title
-label = tk.Label(root, text="Organização de Calendário", font=("Arial", 16))
-label.pack(pady=10)
+# Inicializa as guias
+tab1 = tk.Frame(notebook, bg="lightblue")
+tab2 = tk.Frame(notebook, bg="lightblue")
+tab3 = tk.Frame(notebook, bg="lightblue")
 
-# Error label for displaying error messages
-error_label = tk.Label(root, text="", fg="red", font=("Arial", 12))
-error_label.pack(pady=5)
+# Adiciona o "nome" das guias
+notebook.add(tab1, text="Manutenção")
+notebook.add(tab2, text="Notificação")
+notebook.add(tab3, text="Visualização")
 
-# Text area for displaying the calendar
-calendar_text = tk.Text(root, height=10, width=50)
-calendar_text.pack(pady=10)
+notebook.pack(expand=True, fill="both")
 
-# Function to view the current calendar
+# Declaração das funções
+
+def open_form():
+    form_window = tk.Toplevel(root)
+    form_window.title("Form")
+    form_window.geometry("300x200")
+    
+    icon_path = os.path.join(os.path.dirname(__file__), 'formimage.ico')
+    form_window.iconbitmap(icon_path)
+    
+    tk.Label(form_window, text="Nome:", font=("Arial", 12)).pack(pady=5)
+    name_entry = tk.Entry(form_window, font=("Arial", 12))
+    name_entry.pack(pady=5)
+
+    tk.Label(form_window, text="Email:", font=("Arial", 12)).pack(pady=5)
+    email_entry = tk.Entry(form_window, font=("Arial", 12))
+    email_entry.pack(pady=5)
+    
+    submit_button = tk.Button(form_window, text="Adicionar", font=("Arial", 12), command=lambda: submit_form(name_entry.get(), email_entry.get()))
+    submit_button.pack(pady=20)
+
+def submit_form(name, email):
+    print(f"Name: {name}, Email: {email}")
+
 def view_calendar():
     error_message = empresa.preencherCalendario()
     if error_message:
@@ -39,16 +67,6 @@ def view_calendar():
         for week in empresa.calendario:
             calendar_text.insert(tk.END, f"{week}\n")
 
-# Button to view the calendar
-view_calendar_button = tk.Button(root, text="Ver Calendário", command=view_calendar)
-view_calendar_button.pack(pady=10)
-
-# Entry field for adding a new employee
-new_employee_entry = tk.Entry(root, width=30)
-new_employee_entry.pack(pady=5)
-new_employee_entry.insert(0, "Nome de funcionário")
-
-# Function to add a new employee
 def add_employee():
     new_employee = new_employee_entry.get()
     if new_employee:
@@ -60,37 +78,20 @@ def add_employee():
             error_label.config(text="")  # Clear any previous error 
             new_employee_entry.delete(0, tk.END)  # Clear the entry field
 
-# Button to add a new employee
-add_employee_button = tk.Button(root, text="Adicionar", command=add_employee)
-add_employee_button.pack(pady=5)
-
-# Entry field for removing an employee
-remove_employee_entry = tk.Entry(root, width=30)
-remove_employee_entry.pack(pady=5)
-remove_employee_entry.insert(0, "Nome de funcionário")
-
-# Function to remove an employee
 def remove_employee():
     employee_to_remove = remove_employee_entry.get()
     if employee_to_remove:
         error_message = empresa.retirarFuncionario(employee_to_remove)
         if error_message:
-            error_label.config(text=error_message)  # Show error message
+            error_label.config(text=error_message)  
         else:
             label.config(text=f"Removido: {employee_to_remove}")
-            error_label.config(text="")  # Clear any previous error
-            remove_employee_entry.delete(0, tk.END)  # Clear the entry field
+            error_label.config(text="") 
+            remove_employee_entry.delete(0, tk.END) 
 
-# Button to remove an employee
-remove_employee_button = tk.Button(root, text="Remover", command=remove_employee)
-remove_employee_button.pack(pady=5)
-
-add_subtitle_entry = tk.Entry(root, width=30)
-add_subtitle_entry.pack(pady=5)
-add_subtitle_entry.insert(0, "Titulo")
 
 def create_simple_calendar_pdf():
-    year = datetime.now().year  # Set the year for which the calendar is generated
+    year = datetime.now().year
     size_multiplier = 0.6
     space_x = 100
     space_y = 30
@@ -106,8 +107,8 @@ def create_simple_calendar_pdf():
     c = canvas.Canvas(pdf_file, pagesize=letter)
     width, height = letter
 
-    cols = 2  # Number of columns for months
-    rows = 6  # Number of rows for months
+    cols = 2
+    rows = 6 
     month_width = (width / cols) * size_multiplier
     month_height = (height / rows) * size_multiplier
 
@@ -119,9 +120,9 @@ def create_simple_calendar_pdf():
     c.setFont("Helvetica", 12 * size_multiplier)
     c.drawString((width - subtitle_width) / 2, height - 50 * size_multiplier, subtitle)
 
-    last_week_employee = None  # Track the last week's employee
-    week_counter = 0  # Global week counter to rotate employees
-    employee_rotation = empresa.funcionarios  # Assuming `empresa.funcionarios` is the list of employee names
+    last_week_employee = None
+    week_counter = 0 
+    employee_rotation = empresa.funcionarios
 
     for month in range(1, 13):
         col = (month - 1) % cols
@@ -129,14 +130,12 @@ def create_simple_calendar_pdf():
         x = col * month_width + col * space_x + month_x
         y = height - (row + 1) * month_height - row * space_y - 60 * size_multiplier + month_y
 
-        # Month title
         month_name = calendar.month_name[month]
         c.setFillColor(colors.blue)
         c.rect(x, y + month_height - 20 * size_multiplier, month_width, 20 * size_multiplier, stroke=0, fill=1)
         c.setFillColor(colors.white)
         c.setFont("Helvetica-Bold", 12 * size_multiplier)
 
-        # Center the month title        
         month_title_width = c.stringWidth(month_name, "Helvetica-Bold", 12 * size_multiplier)
         c.drawString(x + (month_width - month_title_width) / 2, y + month_height - 15 * size_multiplier, month_name)
 
@@ -150,7 +149,6 @@ def create_simple_calendar_pdf():
             day_width = c.stringWidth(day, "Helvetica", 10 * size_multiplier)
             c.drawString(x + (i * (month_width / 7)) + (month_width / 7 - day_width) / 2, y + month_height - 35 * size_multiplier, day)
 
-        # Calendar days
         month_calendar = calendar.monthcalendar(year, month)
         y_offset = y + month_height - 60 * size_multiplier
 
@@ -183,7 +181,6 @@ def create_simple_calendar_pdf():
             c.rect(emp_rect_x, emp_rect_y, emp_rect_width, emp_rect_height, stroke=1, fill=0)
             c.drawString(emp_rect_x + (emp_rect_width - string_width) / 2, emp_rect_y + (emp_rect_height - 10 * size_multiplier) / 2, current_employee)
 
-            # Week label for the first week of the month
             if week_index == 0:
                 c.rect(emp_rect_x, emp_rect_y + emp_rect_height + 2, emp_rect_width, emp_rect_height, stroke=1, fill=0)
                 c.drawString(emp_rect_x + (emp_rect_width - string_width) / 2, emp_rect_y + emp_rect_height + 2 + (emp_rect_height - 10 * size_multiplier) / 2, "Nome")
@@ -191,11 +188,29 @@ def create_simple_calendar_pdf():
             y_offset -= 20 * size_multiplier
 
     c.save()
-    label.config(text = f"PDF '{pdf_file}' created successfully!")
+    label.config(text = f"PDF '{pdf_file}'")
 
-create_pdf_button = tk.Button(root, text="Criar PDF do Calendário", command=create_simple_calendar_pdf)
-create_pdf_button.pack(pady=10)
+# Declaração de fontes
+titleFont = ("Arial", 20)
 
+# Tab 1 - Manutenção
+title_tab1 = tk.Label(tab1, text="Manutenção de Funcionários", font=titleFont, bg="lightblue")
+title_tab1.pack(padx=20, pady=20)
+
+button_new_employ = tk.Button(tab1, text="Adicionar Funcionário", command=open_form)
+button_new_employ.pack(padx=30, pady=40)
+
+button_remove_employ = tk.Button(tab1, text="Remover Funcionário")
+button_remove_employ.pack(padx=60, pady=40)
+
+#Tab 2 - Notificação
+title_tab2 = tk.Label(tab2, text="Notificação de Funcionários", font=titleFont, bg="lightblue")
+title_tab2.pack(padx=20, pady=20)
+
+
+#Tab 3 - Visualização
+title_tab3 = tk.Label(tab3, text="Visualização do Calendário e Geração de PDF", font=titleFont, bg="lightblue")
+title_tab3.pack(padx=20, pady=20)
 
 if __name__ == "__main__":
     root.mainloop()
